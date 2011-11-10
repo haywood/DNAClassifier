d51a51c0fb9c8fbe3b01b10e444a36b3d0130111
Michael H Reed <mhr2126@gmail.com>
2011-11-10 14:45:54 -0500

    finished with this

--- matlab/classifier.m
+++ matlab/classifier.m
@@ -6,34 +6,18 @@
 function classifier(trainin, testin, trainout, testout)
     
     [sample_labels, originals] = read_dna(trainin); % read training data
-    samples = transform_dna(originals); % transform it to have useful features
-    n_samples = size(samples, 1);
+    samples = originals;
+    n_samples = size(samples, 2);
     classes = {};
 
     judge = nn_classifier(); % make the training sample
     correct = 0;
 
-    % partition data into training and validation
-    [train_samples, train_labels, validation_samples, test_labels] = split_samples(samples, sample_labels);
-
-    judge = judge.train(train_labels, train_samples);
-    for i = 1:size(validation_samples, 2)
-        prediction = judge.predict(validation_samples{i});
-        if strcmp(prediction, test_labels{i})
-            correct = correct + 1;
-        end
-    end
-
-    correct = correct/size(validation_samples, 2);
-
-    % sort by distance from the mean
-    display 'cross validation results'
-    correct
-    fprintf(1, 'using %d training and %d testing\n', size(train_samples, 2), size(validation_samples, 2));
+    judge = judge.train(sample_labels, samples);
 
     % classify every training sample
     out_file = fopen(trainout, 'w');
-    for i = 1:size(samples, 2)
+    for i = 1:n_samples
         prediction = judge.predict(samples{i});
         fprintf(out_file, '%s', prediction);
         fprintf(out_file, ' %s', originals{i});
@@ -42,7 +26,7 @@ function classifier(trainin, testin, trainout, testout)
     fclose(out_file);
 
     [test_labels, originals] = read_dna(testin); % read test data
-    test_samples = transform_dna(originals); % transform test data to have useful features
+    test_samples = originals;
     out_file = fopen(testout, 'w');
 
     % classify test samples
@@ -53,32 +37,3 @@ function classifier(trainin, testin, trainout, testout)
         fprintf(out_file, '\n');
     end
     fclose(out_file);
-
-% split samples for cross validation
-
-function [train_samples, train_labels, validation_samples, test_labels] = split_samples(samples, labels)
-
-    train_samples = {};
-    train_labels = {};
-    validation_samples = {};
-    test_labels = {};
-
-    for i = 1:size(samples, 2)
-        if ~ismember(labels{i}, train_labels)
-            train_samples{end+1} = samples{i};
-            train_labels{end+1} = labels{i};
-        else
-            k = find(ismember(labels{i}, train_labels) == 1);
-            s = sum(samples{i} == 'N') + sum(samples{i} == '-');
-            c = sum(train_samples{k} == 'N') + sum(train_samples{k} == '-');
-            if s < c % always favor the sample with less missing data
-                validation_samples{end+1} = train_samples{k};
-                test_labels{end+1} = train_labels{k};
-                train_samples{k} = samples{i};
-                train_labels{k} = labels{i};
-            else
-                validation_samples{end+1} = samples{i};
-                test_labels{end+1} = labels{i};
-            end
-        end
-    end
--- matlab/nn_classifier.m
+++ matlab/nn_classifier.m
@@ -3,9 +3,6 @@ classdef nn_classifier
 
     properties
         examples = {}; % training data
-        mean_ = [];
-        cov_ = [];
-        Aw = [];
     end
 
     methods
@@ -13,27 +10,19 @@ classdef nn_classifier
         % this overrides old training data
         function self = train(self, sample_labels, samples)
             self.examples = {};
-            X = [];
             for i = 1:size(samples, 2)
-                x = double(samples{i});
-                self.examples{end+1} = {sample_labels{i}, x'/norm(x)};
-                X = [X; double(samples{i})'];
+                self.examples{end+1} = {sample_labels{i}, samples{i}};
             end
-
-            self.mean_ = mean(X);
-            self.cov_ = cov(X);
-            [D, V] = eig(self.cov_);
-            self.Aw = V*D^-0.5;
         end
 
         % predict the class of a sample
         function prediction = predict(self, x)
             best = {inf, ''};
-            x = double(x);
-            x = x'/norm(x);
             for i = 1:size(self.examples, 2)
                 y = self.examples{i}{2};
-                l = norm(x - y, 4) - x'*y;
+                s = max([min(regexpi(x, '[AGTC]')) min(regexpi(y, '[AGTC]'))]);
+                e = min([min(regexp(x, '-')) min(regexp(y, '-'))]);
+                l = - sum(x(s:e-1) == y(s:e-1));
                 if l < best{1}
                     best = {l, self.examples{i}{1}};
                 end
