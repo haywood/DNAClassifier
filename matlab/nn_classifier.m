ab9214511a211f933ed12ef22dad39f134f2e2dc
Michael H Reed <mhr2126@gmail.com>
2011-11-10 09:14:10 -0500

    finished

--- matlab/nn_classifier.m
+++ matlab/nn_classifier.m
@@ -3,8 +3,9 @@ classdef nn_classifier
 
     properties
         examples = {}; % training data
-        mean_ = []; % mean of training data
-        cov_ = []; % covariance of training data
+        mean_ = [];
+        cov_ = [];
+        Aw = [];
     end
 
     methods
@@ -12,21 +13,29 @@ classdef nn_classifier
         % this overrides old training data
         function self = train(self, sample_labels, samples)
             self.examples = {};
-            self.mean_ = mean(samples);
-            self.cov_ = cov(samples);
-            for i = 1:size(samples, 1)
-                self.examples{end+1} = {sample_labels{i}, (samples(i, :) - self.mean_)./sqrt(diag(self.cov_)')};
+            X = [];
+            for i = 1:size(samples, 2)
+                x = double(samples{i});
+                self.examples{end+1} = {sample_labels{i}, x'/norm(x)};
+                X = [X; double(samples{i})'];
             end
+
+            self.mean_ = mean(X);
+            self.cov_ = cov(X);
+            [D, V] = eig(self.cov_);
+            self.Aw = V*D^-0.5;
         end
 
         % predict the class of a sample
         function prediction = predict(self, x)
             best = {inf, ''};
-            x = (x - self.mean_)./sqrt(diag(self.cov_)');
+            x = double(x);
+            x = x'/norm(x);
             for i = 1:size(self.examples, 2)
-                d = norm(x - self.examples{i}{2});
-                if d < best{1}
-                    best = {d, self.examples{i}{1}};
+                y = self.examples{i}{2};
+                l = norm(x - y, 4) - x'*y;
+                if l < best{1}
+                    best = {l, self.examples{i}{1}};
                 end
             end
             prediction = best{2};
